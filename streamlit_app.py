c7760a371674070ac222862aaf110d0c4d86f917
Ritik Srivastava <ritiksrivastava144@gmail.com>
2022-07-03 16:40:02 +0530

    Error fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ streamlit.text(" 🥗 Kale, Spinach & Rocket Smoothie")
 streamlit.text("🐔 Hard-Boiled Free-Range Egg")
 streamlit.text("🥑🍞 Avacado Toast")
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie')
                  
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)

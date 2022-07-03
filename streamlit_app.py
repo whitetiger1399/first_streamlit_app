7e7056ce3a3cd668d10cda230ffac18ad9d08230
Ritik Srivastava <ritiksrivastava144@gmail.com>
2022-07-03 16:38:59 +0530

    Added my_fruit_list using Pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit 
+import pandas as pd
 
 streamlit.title("My Parents New Healthy Diner")
 streamlit.header("Breakfast Favorites")
@@ -7,5 +8,7 @@ streamlit.text(" 🥗 Kale, Spinach & Rocket Smoothie")
 streamlit.text("🐔 Hard-Boiled Free-Range Egg")
 streamlit.text("🥑🍞 Avacado Toast")
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 
+                 
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)

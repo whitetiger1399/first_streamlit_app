6934f2c3cec2ead2cc0b67a826601968ad57581c
Ritik Srivastava <ritiksrivastava144@gmail.com>
2022-07-03 16:49:58 +0530

    Show selected Fruits dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,5 +13,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie')
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list=my_fruit_list.set_index('Fruit')
 
-streamlit.multiselect("Pick your favourite fruites: ", list(my_fruit_list.index),['Apple'])
-streamlit.dataframe(my_fruit_list)
+fruits_selected=streamlit.multiselect("Pick your favourite fruites: ", list(my_fruit_list.index),['Apple', 'Avocado'])
+fruits_to_show=my_fruit_list.loc[fruits_selected]
+
+streamlit.dataframe(fruits_to_show)

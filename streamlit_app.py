83a7fa5a6276d3b20fff81af7a9e8b99fa3fa817
Ritik Srivastava <ritiksrivastava144@gmail.com>
2022-07-03 16:43:42 +0530

    Added multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,4 +11,7 @@ streamlit.text("🥑🍞 Avacado Toast")
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie')
                  
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list=my_fruit_list.set_index('Fruit')
+
+streamlit.multiselect("Pick your favourite fruites: ", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)

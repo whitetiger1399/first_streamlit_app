4ce9499557d4fc1705ed40c321025f09df5a024f
Ritik Srivastava <ritiksrivastava144@gmail.com>
2022-07-03 16:45:27 +0530

    Added default option for multi select

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,5 +13,5 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie')
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list=my_fruit_list.set_index('Fruit')
 
-streamlit.multiselect("Pick your favourite fruites: ", list(my_fruit_list.index))
+streamlit.multiselect("Pick your favourite fruites: ", list(my_fruit_list.index),['Apple', 'Avacado'])
 streamlit.dataframe(my_fruit_list)

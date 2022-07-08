005815a06bbf62c1f1200bd18ecccb41406f526b
Ritik Srivastava <ritiksrivastava144@gmail.com>
2022-07-08 18:14:57 +0530

    Added choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,4 +23,8 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 fruityvice_normalized=(fruityvice_response.json())
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 streamlit.dataframe(fruityvice_normalized)

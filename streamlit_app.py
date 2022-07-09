71cbaee1a52f54ff46b14e3a5bbd61ba8b99265b
Ritik Srivastava <ritiksrivastava144@gmail.com>
2022-07-09 16:54:10 +0530

    error check

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,6 +38,7 @@ try:
     back_from_function = get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
 
+streamlit.stop()
   
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
